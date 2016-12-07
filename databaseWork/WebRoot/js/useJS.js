a8f6e5722f3dbfed3adb4bff10d3cea36b10bcc1
obligat <duzi1996@qq.com>
2016-12-07 09:45:26 +0800

    update useJS.js

--- databaseWork/WebRoot/js/useJS.js
+++ databaseWork/WebRoot/js/useJS.js
@@ -1,4 +1,4 @@
-function createHttpRequest() {
+function createHttpRequest(method, url, callback) {
 	httpRequest = false;
 	if (window.XMLHttpRequest) {
 		httpRequest = new XMLHttpRequest();
@@ -19,33 +19,15 @@ function createHttpRequest() {
 		return false;
 	}
 
-	return httpRequest;
+	httpRequest.onreadystatechange = callback;
+	httpRequest.open(method, url, true);
+	httpRequest.send(null);
 
 }
 
 function getPublicKey() {
-	httpRequest = false;
-	if (window.XMLHttpRequest) {
-		httpRequest = new XMLHttpRequest();
-	} else if (window.ActiveXObject) {
-		try {
-			httpRequest = new ActiveXObject("Msxm12.XMLHTTP");
-		} catch (e) {
-			try {
-				httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-			} catch (e) {
-			}
-		}
+	createHttpRequest("GET", "GetKey", getPk);
 
-	}
-
-	if (!httpRequest) {
-		alert("can not create XMLHttpRequest object .");
-		return false;
-	}
-	httpRequest.onreadystatechange = getPk;
-	httpRequest.open("GET", "GetKey", true);
-	httpRequest.send(null);
 }
 
 function getPk() {
@@ -53,7 +35,7 @@ function getPk() {
 		if (httpRequest.status == 200) {
 			document.getElementById("publicKey").value = httpRequest.responseText;
 		} else {
-			alert("your request encounter wrong ."+httpRequest.responseText);
+			alert("your request encounter wrong ." + httpRequest.responseText);
 		}
 	}
 }
